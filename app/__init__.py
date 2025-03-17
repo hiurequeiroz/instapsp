bd2ce4beed24fd53dd1a50163b773f853130581d
hiurequeiroz <hiure@riseup.net>
2025-03-17 17:52:30 -0300

    feat: adiciona suporte a CSS admin e ajusta configuração static_url_path

--- app/__init__.py
+++ app/__init__.py
@@ -28,6 +28,7 @@ def create_app(config_class=Config):
     """
     app = Flask(__name__,
         static_folder='static',
+        static_url_path='/static',
         template_folder='templates'
     )
     
